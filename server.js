5eeea98fd1a2b60db532757358c9aeec0f24e8f2
pfgallagher <pfgallagher@gmail.com>
2019-04-14 21:53:35 -0500

    Finish project

--- server.js
+++ server.js
@@ -1,37 +1,75 @@
-'use strict';
+const express = require("express");
+const mongo = require("mongodb");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const isUrl = require("is-url");
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+const app = express();
+const port = process.env.PORT || 3000;
 
-var cors = require('cors');
+const Schema = mongoose.Schema;
 
-var app = express();
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
-// Basic Configuration 
-var port = process.env.PORT || 3000;
-
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+const URL = mongoose.model(
+	"URL",
+	new Schema({
+		originalUrl: {
+			type: String,
+			required: true,
+		},
+	}),
+);
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
-
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", (req, res) => {
+	res.sendFile(`${process.cwd()}/views/index.html`);
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/:shortUrl", async (req, res, next) => {
+	try {
+		const shortUrl = await URL.findById(req.params.shortUrl);
+		if (shortUrl) res.redirect(`${shortUrl.originalUrl}`);
+		else res.status(404).send("404: The URL you provided does not exist.");
+	} catch (error) {
+		next(error);
+	}
 });
 
+app.post("/api/shorturl/new", async (req, res, next) => {
+	try {
+		const { url } = req.body;
+		if (isUrl(url)) {
+			const alreadyInDB = await URL.findOne({ originalUrl: url });
 
-app.listen(port, function () {
-  console.log('Node.js listening ...');
+			if (alreadyInDB)
+				res.json({
+					original_URL: alreadyInDB.originalUrl,
+					short_url: alreadyInDB._id,
+				});
+			else {
+				const newURL = await URL.create({
+					originalUrl: url,
+				});
+				res.json({
+					original_URL: newURL.originalUrl,
+					short_url: newURL._id,
+				});
+			}
+		} else res.json({ error: "Invalid URL" });
+	} catch (error) {
+		next(error);
+	}
+});
+
+app.get("/api/hello", (req, res) => {
+	res.json({ greeting: "hello API" });
+});
+
+app.listen(port, () => {
+	console.log("Node.js listening ...");
 });
